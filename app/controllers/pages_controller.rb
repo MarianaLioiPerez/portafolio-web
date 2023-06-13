0252bb6ee5753d3bdb971776c089440802455d24
MarianaLioiPerez <lioiperezm@gmail.com>
2023-06-12 21:52:08 -0400

    relacion de rutas

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def proyectos
+  end
+
+  def contacto
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  #get 'pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get "proyectos", to: 'pages#proyectos'
+  get "contacto", to: 'pages#contacto'
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#home"
 end
